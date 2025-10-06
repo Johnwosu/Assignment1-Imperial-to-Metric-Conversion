abceab7b9a2fdeee2fa91bf4e0780828d175f5e8
John Nwosu <nwosujohnifeanyi@gmail.com>
2025-10-06 04:37:36 -0300

    Updated

--- ImperialToMetricConversion.py
+++ ImperialToMetricConversion.py
@@ -5,6 +5,7 @@
 # 2. Input for stones
 # 3. Input for pounds
 # 4. Input for ounces
+tons = ()
 
 #--Required Formula----
 #--total ounces = 35840 * tons + 224 * stone + 16 * pounds + ounces
@@ -19,3 +20,10 @@
 # 5. grams = (totalKilos - (metricTons * 1000 + remainingKilos)) * 1000
 
 #---Output-----
+# print
+#------Input--------
+tons = int(input("Enter the number of imperial tons: "))
+stones = input("Enter the number of stones: ")
+pounds = float(input("Enter the number of pounds: "))
+ounces = float(input("Enter the number of ounces: "))
+
