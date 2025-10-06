720301b32dc13d5d00a143c836d3b6e7e65beaa9
John Nwosu <nwosujohnifeanyi@gmail.com>
2025-10-06 05:56:52 -0300

    Ready for Marking

--- ImperialToMetricConversion.py
+++ ImperialToMetricConversion.py
@@ -5,7 +5,6 @@
 # 2. Input for stones
 # 3. Input for pounds
 # 4. Input for ounces
-tons = ()
 
 #--Required Formula----
 #--total ounces = 35840 * tons + 224 * stone + 16 * pounds + ounces
@@ -20,9 +19,10 @@ tons = ()
 # 5. grams = (totalKilos - (metricTons * 1000 + remainingKilos)) * 1000
 
 #---Output-----
-# print
+# print results
+
 #------Input--------
-tons = int(input("Enter the number of imperial tons: "))
+tons = int(input("Enter the number of tons: "))
 stones = float(input("Enter the number of stones: "))
 pounds = float(input("Enter the number of pounds: "))
 ounces = float(input("Enter the number of ounces: "))
@@ -31,7 +31,6 @@ ounces = float(input("Enter the number of ounces: "))
 totalOunces = 35840 * tons + 224 * stones + 16 * pounds + ounces
 totalKilos = totalOunces / 35.274
 
-
 #--Convert to ounces--
 totalOunces = (35840 * tons) + (224 * stones) + (16 * pounds) + ounces
 
@@ -39,10 +38,22 @@ totalOunces = (35840 * tons) + (224 * stones) + (16 * pounds) + ounces
 totalKilos = totalOunces / 35.274
 
 #--Analysing metric tons, kilos and grams---#
+
+# the operator // rounds to whole numbers (integars)
 metricTons = int(totalKilos // 1000)
-remainingKilos = int(totalKilos % 1000)
+
+# the operator % returns the remaining integar value after getting the metric tons
+remainingKilos = int(totalKilos % 1000) 
 grams = (totalKilos - (metricTons * 1000 + remainingKilos)) * 1000
 
 #<-----Output--------->
-print("The metric weight is {0} metric tons, {1} kilos, and {2:.1f} grams".format(metricTons, remainingKilos, grams))
-
+print()
+print("Imperial to Metric Conversion")
+print()
+print("Enter the number of tons: {0}".format(tons))
+print("Enter the number of stones: {0}".format(int(stones)))
+print("Enter the number of pounds: {0}".format(int(pounds)))
+print("Enter the number of ounces: {0}".format(int(ounces)))
+print()
+print("The metric weight is {0} metric tons, {1} kilos, and {2:.1f} grams.".format(metricTons, remainingKilos, grams))
+print()
